fa67450c929b0aa0a4f52c0d264990786eee0b54
VShaliagin <vshalyagin@mail.ru>
2020-01-27 20:10:31 +1000

    Попытка перезалить задания HW_1

--- task_3.py
+++
@@ -1,9 +0,0 @@
-print('#' * 20 + ' Ex_3 ' + '#' * 20)
-q_number = 'Введите целое число.\n'
-var_1 = input(q_number)
-var_2 = var_1 * 2
-var_3 = var_1 * 3
-
-user_summ = int(var_1) + int(var_2) + int(var_3)
-
-print(f'Сумма равняется: {user_summ}')
