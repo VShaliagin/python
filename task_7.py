f93cfefd16f400b4a010194674a871dc7d60030a
VShaliagin <vshalyagin@mail.ru>
2020-02-10 21:50:26 +1000

    Дописал 7е задание по уроку

--- task_7.py
+++ task_7.py
@@ -15,7 +15,32 @@ firm_1 ООО 10000 5000.
 
 import os.path as path
 import json
-base_path = path.join(path.dirname(__file__), 'task_7.json')
+import statistics
+
+base_path = path.join(path.dirname(__file__), 'task_7.txt')
+
+firms = dict()
+
+with open(base_path, 'r', encoding='UTF-8') as file:
+    for line in file:
+        tmp = line.split(' ')
+        name = tmp[0]
+        a, b = int(tmp[2]), int(tmp[3]),
+        firms[name] = a - b
+print(firms)
+
+'average profit'
+
+result = []
+result.append(firms)
+
+result.append({'average profit':statistics.mean([itm for itm in firms.values() if itm>=0])})
+
+print(result)
+
+with open(path.join(path.dirname(__file__), 'task_7.json'), 'w') as file:
+    file.write(json.dumps(result))
+
 
 # with open(base_path, 'r', encoding='UTF-8') as file:
 #     templates = file.read()
@@ -27,18 +52,28 @@ base_path = path.join(path.dirname(__file__), 'task_7.json')
 #     print(section)
 #     print('\n'.join(commands))
 
-my_dict={ }
-avg_cash = 0
-i=0
 
 
-with open(base_path, 'r', encoding='UTF-8') as file:
-    for line in file:
-        line_elem = line.split(' ')
-        if int(line_elem[2]) - int(line_elem[3])>0:
-            profit =int(line_elem[2]) - int(line_elem[3])
-            i += 1
-            print(profit)
-    print(profit/i)
+
+
 
 #
+#
+# my_dict=[]
+# avg_cash = 0
+# i=0
+# open("my_file.json", "w")
+#
+#
+# with open(base_path, 'r', encoding='UTF-8') as file:
+#     for line in file:
+#         line_elem = line.split(' ')
+#         with open("my_file.json", "a") as write_f:
+#             json.dumps(line_elem)
+
+    #     if int(line_elem[2]) - int(line_elem[3])>0:
+    #         profit =int(line_elem[2]) - int(line_elem[3])
+    #         i += 1
+    #         print(profit)
+    # print(profit/i)
+
