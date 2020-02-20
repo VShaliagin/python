ae317e3c5554c46d08d66af26622ad56daf7bf76
VShaliagin <vshalyagin@mail.ru>
2020-02-21 00:17:09 +1000

    дополнено

--- task_4.py
+++ task_4.py
@@ -1,8 +1,5 @@
 '''
-5. Продолжить работу над первым заданием.
-Разработать методы, отвечающие за приём оргтехники на склад и передачу в определенное подразделение компании.
-Для хранения данных о наименовании и количестве единиц оргтехники, а также других данных, можно использовать любую
-подходящую структуру, например словарь.
+
 6. Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых пользователем данных.
  Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
 Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей,
@@ -24,14 +21,24 @@ class Warehouse:
 
     def add_warehouse(self, equipment):
         self.__box .append(equipment)
+    def transfer_to(self, equipment):
+        if not equipment in self.__box:
+            raise KeyError(equipment)
+        self.__box.remove(equipment)
+    def move_to(self, equipment, other):
+        self.transfer_to(equipment)
+        other.add_warehouse(equipment)
+
     # Для обращения к списку по индексу
     def __getitem__(self, item):
         return self.__box[item]
     #Для прохода по циклу for
     def __iter__(self):
+        if len(self.__box) == 0:
+            print (f'Список пуст')
         return self.__box.__iter__()
 
-#объявил класс оргтехника
+#класс оргтехника
 class Office_equipment:
     def __init__(self, brand, color, coast, quantity):
         self.brand = brand
@@ -42,7 +49,6 @@ class Office_equipment:
         return f'Модель {self.brand} цвет {self.color} стоимость {self.coast} количество {self.quantity}'
 
 class Printer(Office_equipment):
-
     def __init__(self, brand, color, coast, quantity, type_print, max_size_paper):
         super().__init__(brand, color, coast, quantity)
         self.type_print = type_print
@@ -73,17 +79,23 @@ xerox_2 = Xerox('HP', 'white', 300000, 1, 'А3', '600 DPI')
 warehouse_1 = Warehouse('склад')
 warehouse_2 = Warehouse('офис')
 
-
 # Добавить элементы
 
 warehouse_1.box = print_hp
 warehouse_1.box = print_canon
 warehouse_1.box = scan_plustek
 warehouse_1.box = xerox_2
-
-
+print(f'Склад содержит следующие позиции:')
 for el in warehouse_1:
     print(el)
-#
-# for el in warehouse_2:
-#     print(el)
+print(f'\nВ офисе находятся:')
+for el in warehouse_2:
+    print(el)
+# Передадим в офис print_hp. Проверим на отсутствие элемента на складе и присутствие в офисе
+warehouse_1.move_to(print_hp, warehouse_2)
+print(f'\nПосле передачи на складе')
+for el in warehouse_1:
+    print(el)
+print(f'\nВ офисе')
+for el in warehouse_2:
+    print(el)
